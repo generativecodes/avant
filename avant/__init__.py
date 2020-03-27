7199cfd5969bb431cb826ecdff19fda54c5d4e5d
generativecodes <50173603+generativecodes@users.noreply.github.com>
2020-03-27 12:36:19 -0500

    cleaned

--- avant/__init__.py
+++ avant/__init__.py
@@ -1,29 +1,3 @@
-# dependencies
-#import cairo
-#import gi
-#gi.require_version("Gtk", "3.0")
-#from gi.repository import Gtk, Gdk, GLib
-
-
-#from avant.Vector import Vector
-#from avant.settings import *
-#from avant.forms import *
-
 from avant.main import main
 
 main()
-
-#import sys
-#from os import path
-#import __main__
-
-#win = Gtk.Window()
-#win.connect('destroy', Gtk.main_quit)
-#
-#FILEPATH = path.abspath(sys.modules['__main__'].__file__)
-#drawingarea = Animator(win, FILEPATH)
-
-#win.add(drawingarea)
-
-#win.show_all()
-#Gtk.main()
