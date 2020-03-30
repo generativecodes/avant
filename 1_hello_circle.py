feec40959e935d0550caea9938a3f77f8e4bf201
generativecodes <generativecodes@users.noreply.github.com>
2020-03-30 14:34:43 -0500

    circle animation example

--- 1_hello_circle.py
+++ 1_hello_circle.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 import avant
-from math import sin, cos
-
-x = 0
 
 def setup():
     pass
 
 def loop():
-    global x
-    x += 0.1
-    position_x = SIZE * sin(x)
-    position_y = SIZE * cos(x)
-    circle(position_x, position_y)
+    circle()
 
